d057e803ba4b91a5920743a25fe9e773f66ffd4c
wolvg <57114116+wolvg@users.noreply.github.com>
2019-11-03 18:27:52 +0100

    Add files via upload

---
+++ EX17_Palindrome.py
@@ -0,0 +1,22 @@
+def palindrome(mot):
+    Y = 'est un palindrome.'
+    N = 'n\'est pas un palindrome'
+
+    Temp1 = 0
+    Temp2 = 0
+
+    C = mot
+    C2 = C[::-1]
+    l = [i for i in C]
+    l2 = [i for i in C2]
+
+    if C == C2:
+        return Y
+
+    else:
+        return N
+
+A = input('Entrez un mot : ')
+
+X = palindrome(A)
+print('Le mot \"',A,'\"',X)
